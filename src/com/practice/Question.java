fe40e61e295b67a974131bd49c6f5c72ed62f568
Ashish <ashishmathew.77@gmail.com>
2022-02-23 21:48:05 -0600

    names.txt added

---
+++ src/com/practice/Question.java
@@ -0,0 +1,21 @@
+package com.practice;
+
+import java.util.Arrays;
+public class Question {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Use this space for two pointer problems
+		 */
+		TwoPointer ans = new TwoPointer();
+		System.out.println("The two numbers are :" + Arrays.toString(ans.twoSum(new int[] { -3, 8, -8, 4, 3 }, 7)));
+		
+		/*
+		 * Use this space for Heap Problems
+		 */
+		
+		
+	}
+
+}
---
+++ src/com/practice/TwoPointer.java
@@ -0,0 +1,33 @@
+package com.practice;
+
+import java.util.Arrays;
+
+class TwoPointer {
+	/*
+	 * This is the solution to the two sum problem
+	 */
+	public int[] twoSum(int[] nums, int target) {
+		// TODO Auto-generated method stub
+		Arrays.sort(nums);
+		int i = 0;
+		int j = nums.length - 1;
+		while (i < j) {
+			int first = nums[i];
+			int second = nums[j];
+			if (first + second == target)
+				return new int[] { first, second };
+			/*
+			 * If the the sum is lesser than the target move the left pointer
+			 * to the right
+			 */
+			else if (first + second < target)
+				i++;
+			/*
+			 * else move the right pointer to the left
+			 */
+			else
+				j--;
+		}
+		return new int[0];
+	}
+}
