de415afce910c73d3b735687c9a473238a6b7226
Ashish <ashishmathew.77@gmail.com>
2022-02-27 09:52:08 -0600

    added sliding windown median problem

--- src/com/practice/MedianOfStream.java
+++ src/com/practice/MedianOfStream.java
@@ -1,12 +1,15 @@
 package com.practice;
+
+import java.util.Collections;
 import java.util.PriorityQueue;
+
 class MedianOfStream {
 	PriorityQueue<Integer> maxHeap;
 	PriorityQueue<Integer> minHeap;
 
 	public MedianOfStream() {
-		maxHeap = new PriorityQueue<Integer>((a, b) -> b - a);
-		minHeap = new PriorityQueue<Integer>((a, b) -> a - b);
+		maxHeap = new PriorityQueue<Integer>();
+		minHeap = new PriorityQueue<Integer>(Collections.reverseOrder());
 	}
 
 	public void insertNum(int num) {
@@ -20,7 +23,7 @@ class MedianOfStream {
 		/*
 		 * check imbalance in the heaps
 		 */
-		
+
 		if (maxHeap.size() > minHeap.size() + 1)
 			minHeap.add(maxHeap.poll());
 		else if (maxHeap.size() < minHeap.size())
@@ -29,9 +32,70 @@ class MedianOfStream {
 	}
 
 	public double findMedian() {
-		if(maxHeap.size() == minHeap.size())
-			return ((double)maxHeap.peek() + (double)minHeap.peek()) / 2.0;
-		else 
+		if (maxHeap.size() == minHeap.size())
+			return ((double) maxHeap.peek() + (double) minHeap.peek()) / 2.0;
+		else
 			return maxHeap.peek();
 	}
+}
+
+class SlidingWindowMedian {
+
+	PriorityQueue<Integer> minHeap;
+	PriorityQueue<Integer> maxHeap;
+
+	public SlidingWindowMedian() {
+		// TODO Auto-generated constructor stub
+		minHeap = new PriorityQueue<Integer>();
+		maxHeap = new PriorityQueue<Integer>(Collections.reverseOrder());
+	}
+
+	public double[] slidingWindowMedian(int[] nums, int k) {
+		//insert
+		double[] result = new double[nums.length -k + 1];
+		for(int i = 0 ; i < nums.length ; i++) {
+			
+			if(maxHeap.isEmpty() || maxHeap.peek() >= nums[i])
+				maxHeap.add(nums[i]);
+			else
+				minHeap.add(nums[i]);
+			
+			//rebalance the heap
+			rebalanceHeaps();	
+		/*
+		 * 1.  Check window
+		 * 1a. Find median
+		 * 1b. Remove the element from the sliding window
+		 * 1c. rebalance the heap
+		 */
+		
+			if(i-k+1 >= 0) {
+				if(maxHeap.size() == minHeap.size())
+					result[i-k+1] = ((double)minHeap.peek() + (double)maxHeap.peek())/2.0;
+				else
+					result[i-k+1] = maxHeap.peek();
+				
+				int elementToBeRemoved = nums[i-k+1];
+				if(elementToBeRemoved <= maxHeap.peek())
+					maxHeap.remove(elementToBeRemoved);
+				else
+					minHeap.remove(elementToBeRemoved);
+				
+				rebalanceHeaps();
+				
+			}
+			
+		}
+		return result;
+	}
+
+	public void rebalanceHeaps() {
+		
+		if(maxHeap.size() > minHeap.size() + 1)
+			minHeap.add(maxHeap.poll());
+		else if (minHeap.size() > maxHeap.size())
+			maxHeap.add(minHeap.poll());
+		
+	}
+
 }
--- src/com/practice/Question.java
+++ src/com/practice/Question.java
@@ -2,9 +2,6 @@ package com.practice;
 
 import java.util.Arrays;
 
-
-
-
 public class Question {
 
 	public static void main(String[] args) {
@@ -24,14 +21,28 @@ public class Question {
 		 * Use this space for Heap Problems
 		 */
 
-		MedianOfStream median = new MedianOfStream();
-		median.insertNum(3);
-		median.insertNum(1);
-		System.out.println("Median : " + median.findMedian());
-		median.insertNum(5);
-		System.out.println("Median : " + median.findMedian());
-		median.insertNum(4);
-		System.out.println("Median : " + median.findMedian());
+		// Find median of a stream
+		/*
+		 * MedianOfStream median = new MedianOfStream(); median.insertNum(3);
+		 * median.insertNum(1); System.out.println("Median : " + median.findMedian());
+		 * median.insertNum(5); System.out.println("Median : " + median.findMedian());
+		 * median.insertNum(4); System.out.println("Median : " + median.findMedian());
+		 */
+
+		// Sliding window median
+
+		SlidingWindowMedian median = new SlidingWindowMedian();
+		double[] result = median.slidingWindowMedian(new int[] { 1, 2, -1, 3, 5 }, 2);
+		for (double i : result) {
+			System.out.println(i + " ");
+		}
+		System.out.println();
+
+		median = new SlidingWindowMedian();
+		result = median.slidingWindowMedian(new int[] { 1, 2, -1, 3, 5 }, 3);
+		for (double i : result) {
+			System.out.println(i + " ");
+		}
 
 	}
 
